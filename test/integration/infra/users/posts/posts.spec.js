47d1a9038f9e703860787ee85ca79fdf48ba2d78
srodrigo <sergio.rodrigo.royo@gmail.com>
2016-07-20 11:19:36 +0100

    Move posts integration tests into infra folder

--- test/integration/infra/users/posts/posts.spec.js
+++ test/integration/infra/users/posts/posts.spec.js
@@ -2,8 +2,8 @@ var chai = require('chai');
 var expect = chai.expect;
 var should = require('should');
 
-var Posts = require('./../../../../src/infra/users/posts/posts');
-var clock = require('./../../../infra/test-clock');
+var Posts = require('./../../../../../src/infra/users/posts/posts');
+var clock = require('./../../../../infra/test-clock');
 var constNow = clock.constNow;
 
 describe('Posts', function() {

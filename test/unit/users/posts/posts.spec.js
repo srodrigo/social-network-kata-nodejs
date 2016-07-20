f0d1c02415574942f9d5d1d1c33802e54c917b3b
srodrigo <sergio.rodrigo.royo@gmail.com>
2016-07-20 09:50:55 +0100

    Improve tests readability by separating setup/act/assert sections

--- test/unit/users/posts/posts.spec.js
+++ test/unit/users/posts/posts.spec.js
@@ -11,6 +11,7 @@ describe('Posts', function() {
 		var posts = Posts(clock);
 		var date = constNow();
 		clock.append(date);
+
 		posts.create('Post message', 'username');
 
 		var allPosts = posts.all();
@@ -27,6 +28,7 @@ describe('Posts', function() {
 		posts.create('Another post message', 'another-username');
 
 		var byUsername = posts.byUsername('username');
+
 		byUsername.length.should.equal(1);
 		byUsername[0].should.eql({message: 'Post message', username: 'username', date: firstDate});
 	});
